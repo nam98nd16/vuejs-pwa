9a218ea26ddfaac8607dab70d7de81df4d55d46c
nam98nd16 <nam98nd16@gmail.com>
2020-04-22 14:08:53 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
+    <h1>App version: v1.13.0</h1>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="prompt">
+      <div>
+        A new version is found. Refresh to load it?
+      </div>
+      <div
+        @click="upgrade"
+      >
+        Refresh
+      </div>
+      <div
+        @click="prompt = false"
+      >
+        Later
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +28,27 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  created() {
+    console.log("created");
+    if (this.$workbox) {
+      this.$workbox.addEventListener("waiting", () => {
+        console.log("here");
+        this.prompt = true;
+      });
+    }
+  },
+
+  methods: {
+    async upgrade() {
+      this.prompt = false;
+      await this.$workbox.messageSW({ type: "SKIP_WAITING" });
+    }
+  },
+   data() {
+    return {
+      prompt: false
+    };
   }
 }
 </script>
